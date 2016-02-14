85cdbe630a1fd096853170d80d676b6de89c5eee
larse514 <andrew.larse514@gmail.com>
2016-02-13 20:53:26 -0600

    adding get

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 var issues = require('../service/issues.js');
 
-
+//Non secure services
 router.get('/issues', issues.getAll);
 
 module.exports = router;
